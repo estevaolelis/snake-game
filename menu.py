2870f18b8f77dac1485e78f3bda2034c4a3d67d1
kikkask <sirbritolucas@gmail.com>
2026-06-05 20:23:37 -0300

    mudança no icone do mouse on hover no menu

--- menu.py
+++ menu.py
@@ -18,19 +18,27 @@ class MenuPrincipal:
 
         try:
             imagem_original = pygame.image.load("Graphics/btn_iniciar.png").convert_alpha()
-            self.imagem_botao = pygame.transform.smoothscale(imagem_original, (400, 152))
+            self.imagem_botao_padrao = pygame.transform.smoothscale(imagem_original, (400, 152))
         except FileNotFoundError:
-            self.imagem_botao = pygame.Surface((400, 152))
-            self.imagem_botao.fill((0, 200, 50))
+            self.imagem_botao_padrao = pygame.Surface((400, 152))
+            self.imagem_botao_padrao.fill((0, 200, 50))
 
-        pos_x = (self.largura - self.imagem_botao.get_width()) // 2
-        pos_y = (self.altura - self.imagem_botao.get_height()) // 2
-        self.rect_botao = self.imagem_botao.get_rect(topleft=(pos_x, pos_y))
+        try:
+            imagem_hover_original = pygame.image.load("Graphics/btn_iniciar_hover.png").convert_alpha()
+            self.imagem_botao_hover = pygame.transform.smoothscale(imagem_hover_original, (400, 152))
+        except FileNotFoundError:
+            self.imagem_botao_hover = self.imagem_botao_padrao
+
+        self.imagem_atual = self.imagem_botao_padrao
+
+        pos_x = (self.largura - self.imagem_botao_padrao.get_width()) // 2
+        pos_y = (self.altura - self.imagem_botao_padrao.get_height()) // 2
+        self.rect_botao = self.imagem_botao_padrao.get_rect(topleft=(pos_x, pos_y))
 
     def desenhar(self):
-        """Renderiza o fundo e o botão na tela."""
+        """Renderiza o fundo e o botão na tela baseando no estado atual."""
         self.screen.blit(self.fundo, (0, 0))
-        self.screen.blit(self.imagem_botao, self.rect_botao.topleft)
+        self.screen.blit(self.imagem_atual, self.rect_botao.topleft)
         pygame.display.update()
 
     def executar(self):
@@ -38,6 +46,15 @@ class MenuPrincipal:
         clock = pygame.time.Clock()
         
         while self.rodando:
+            posicao_mouse = pygame.mouse.get_pos()
+            
+            if self.rect_botao.collidepoint(posicao_mouse):
+                self.imagem_atual = self.imagem_botao_hover
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_HAND)
+            else:
+                self.imagem_atual = self.imagem_botao_padrao
+                pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -46,6 +63,7 @@ class MenuPrincipal:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         if self.rect_botao.collidepoint(event.pos):
+                            pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
                             self.rodando = False
 
             self.desenhar()
